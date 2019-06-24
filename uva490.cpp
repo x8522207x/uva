a9381924f79feb877df82933ff8b3a51f952191d
x8522207x <x8522207x@gmail.com>
2019-06-24 21:07:17 +0800

    Create uva490.cpp
    Rotating Sentences

---
+++ uva490.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+using namespace std;
+int main(){
+	vector< vector<char> > test;
+	string a;
+	int line=0;
+	while(getline(cin,a)){
+		test.push_back(vector<char>());
+		for(int i=0;i<a.length();i++){
+			test[line].push_back(a[i]);
+		}
+		line++;
+	}
+	int max=0;
+	for(int i=0;i<line;i++){
+		if(test[i].size()>max){
+			max=test[i].size();
+		}
+	}
+	for(int i=0;i<line;i++){
+		test[i].resize(max);
+	}
+	for(int j=0;j<max;j++){
+		for(int i=line-1;i>=0;i--){
+			if(test[i][j]){
+				cout<<test[i][j];
+			}else if(i!=0 &&!test[i][j]){
+				cout<<" ";
+			}
+		}
+		cout<<'\n';
+	}
+	return 0;
+}

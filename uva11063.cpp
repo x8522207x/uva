268809f5e155c64ea884d167e5f1962220a6cd63
x8522207x <x8522207x@gmail.com>
2019-06-26 10:36:28 +0800

    B2-Sequence*CPE48-23

--- uva11063.cpp
+++ uva11063.cpp
@@ -1,72 +1,44 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
-int main() {
-	int num;
-	int Case = 1;
-	while (cin >> num) {
-		vector<int>nums;
-		bool right = true;
-		for (int i = 0; i<num; i++) {
-			int a;
-			cin >> a;
-			nums.push_back(a);
+int main(){
+	int num=0;
+	int Case=0;
+	while(cin>>num){
+		Case++;
+		int num1=0;
+		vector<int>q;
+		vector<int>answer;
+		for(int i=0;i<num;i++){
+			cin>>num1;
+			q.push_back(num1);
 		}
-		for (int i = 0; i<num - 1; i++) {
-			if (nums[i] >= nums[i + 1]) {
-				right = false;
-				break;
+		for(int i=0;i<q.size()-1;i++){
+			for(int y=i+1;y<q.size();y++){
+				answer.push_back(q[i]+q[y]);
 			}
 		}
-		if (right == true) {
-			for (int i = 0; i<num; i++) {
-				for (int j = i; j<num; j++) {
-					for (int c = i; c<num; c++) {
-						if (c == i) {
-							if (j != num - 1) {
-								for (int d = j + 1; d<num; d++) {
-									if (nums[i] + nums[j] == nums[c] + nums[d]) {
-										right = false;
-										break;
-									}
-								}
-							}
-							else {
-								continue;
-							}
-						}
-						else {
-							for (int d = c; d<num; d++) {
-								if (nums[i] + nums[j] == nums[c] + nums[d]) {
-									right = false;
-									break;
-								}
-							}
-						}
-						if (right == false) {
-							break;
-						}
-					}
-					if (right == false) {
-						break;
-					}
-				}
-				if (right == false) {
+		sort(answer.begin(),answer.end());
+		int g=0;
+		for(int i=0;i<answer.size()-1;i++){
+			for(int y=i+1;y<answer.size();y++){
+				if(answer[i]==answer[y]){
+					g=1;
+					cout<<"Case #"<<Case<<": It is not a B2-Sequence."<<endl;
 					break;
 				}
 			}
+			if(g==1){
+				break;
+			}
 		}
-		if (right == false) {
-			cout << "Case #" << Case << ": It is not a B2-Sequence." << endl;
+		if(g!=1){
+			cout<<"Case #"<<Case<<": It is a B2-Sequence."<<endl;
 		}
-		else {
-			cout << "Case #" << Case << ": It is a B2-Sequence." << endl;
-		}
-		cout << endl;
-		Case++;
+		cout<<endl;
 	}
-
-
 	return 0;
+
 
 }

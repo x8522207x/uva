ea31436903fe4ad13065450afeebe2735f1a6bac
x8522207x <x8522207x@gmail.com>
2019-07-03 21:29:45 +0800

    Decode the Mad man

--- uva10222.cpp
+++ uva10222.cpp
@@ -1,8 +1,6 @@
-#include<iostream>
-#include <string>
+#include <iostream>
 using namespace std;
 int main(){
-
 	string A = "1234567890qwertyuiopasdfghjklzxcvbnm ";
 	string B = "34567890-=ertyuiop[]dfghjkl;'cvbnm,. ";
 	string B1 = "34567890_+ERTYUIOP{}DFGHJKL:'CVBNM<> ";
@@ -10,18 +8,11 @@ int main(){
 	while (getline(cin, C)&&C.length() > 0){
 		for (int i = 0; i < C.length(); i++){
 			for (int j = 0; j < B.length(); j++){
-				if (C[i] == B[j] || C[i] == B1[j]){ cout << A[j]; }
-
+				if (C[i] == B[j] || C[i] == B1[j]){ 
+					cout << A[j]; 
+				}
 			}
 		}
 		cout << endl;
 	}
-
-
-	return 0;
-
-
-
-
-
 }

60e8489931aadd67c13318dced53279196d31feb
x8522207x <x8522207x@gmail.com>
2019-07-03 21:13:00 +0800

    What's Cryptanalysis?

--- uva10008.cpp
+++ uva10008.cpp
@@ -1,53 +1,53 @@
 #include <iostream>
-#include <string>
-#include<cmath>
+#include <algorithm>
+#include <vector>
+#include <sstream>
 using namespace std;
 int main(){
-	int Case;
-	cin >> Case;
-	cin.get();
-	int a[26] = { 0 };
-	int b[26] = { 0 };
-	string Beword = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	string Seword = "abcdefghijklmnopqrstuvwxyz";
-	for (int i = 0; i < 26; i++){
-		b[i] = i;
-	}
-	for (int i = 0; i < Case;i++){
-		string word;
-		getline(cin,word);
-		for (int c = 0; c <word.length(); c++){
-			for (int b = 0; b < 26; b++){
-				if (word[c] == Beword[b] || word[c] == Seword[b]){
-					a[b] = a[b] + 1;
+	string big="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	string small="abcdefghijklmnopqrstuvwxyz";
+	int num[26]={0};
+	stringstream ss;
+	int Case=0;
+	string test;
+	getline(cin,test);
+	ss<<test;
+	ss>>Case;
+	while(Case--){
+		string s;
+		getline(cin,s);
+		for(int i=0;i<s.length();i++){
+			if(s[i]>64&&s[i]<91){
+				for(int j=0;j<big.length();j++){
+					if(s[i]==big[j]){
+						num[j]+=1;
+						break;
+					}
+				}
+			}else if(s[i]>96&&s[i]<123){
+				for(int j=0;j<small.length();j++){
+					if(s[i]==small[j]){
+						num[j]+=1;
+						break;
+					}
 				}
 			}
-			
 		}
 	
 	}
-	for (int s = 0; s < 25; s++){
-		for (int d = s + 1; d < 26; d++){
-			if (a[s]< a[d]){
-				swap(a[s], a[d]);
-				swap(Beword[s], Beword[d]);
-				swap(b[s], b[d]);
-			}
-			else if (a[s] == a[d] && b[s]>b[d]){
-				swap(Beword[s], Beword[d]);
-				swap(b[s], b[d]);
-
+	int max=0;
+	for(int i=0;i<26;i++){
+		if(num[i]!=0&&max<num[i]){
+			max=num[i];
+		}
+	}
+	for(int i=max;i>0;i--){
+		for(int j=0;j<26;j++){
+			if(num[j]==i){
+				cout<<big[j]<<" "<<num[j]<<endl;
 			}
 		}
 	}
-	
-	for (int s = 0; s < 26; s++){
-		if (a[s]>0){
-			cout << Beword[s] << " " << a[s] ;
-			if (s < 25){ cout << endl; }
-		}
-		
-	}
 
-	return 0;
+
 }

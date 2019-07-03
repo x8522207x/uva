ffdcd93ccf3f8c50433408ecee1dd34f192b7bdc
x8522207x <x8522207x@gmail.com>
2019-07-03 22:04:05 +0800

    The Hotel with Infinite Rooms

--- uva10170.cpp
+++ uva10170.cpp
@@ -1,26 +1,16 @@
 #include <iostream>
 using namespace std;
-int main() {
-
-	long long int people, day;
-	while (cin >> people >> day) {
-		long long int now = 0;
-		long long int a = people;
-		for (long long int i = people;; i++) {
-			now = now + i;
-			if (now == day) {
-				cout << a << endl;
+int main(){
+	long long int peoples;
+	long long int day;
+	while(cin>>peoples>>day){
+		long long int sum=0;
+		for(long long int i=peoples;;i++){
+			sum+=i;
+			if(sum>=day){
+				cout<<i<<endl;
 				break;
 			}
-			else if (now > day) {
-				cout << a << endl;
-				break;
-			}
-			a++;
 		}
 	}
-	system("pause");
-	return 0;
-
-
 }

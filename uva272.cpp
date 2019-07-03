754f8304b531526d1f65f91273072bd482a1a777
x8522207x <x8522207x@gmail.com>
2019-07-03 21:48:36 +0800

    TeX Quotes

--- uva272.cpp
+++ uva272.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
-#include <string>
 using namespace std;
 int main(){
 	string A;
 	int change = 0;
 	while (getline(cin, A)){
-		
 		for (int i = 0; i < A.length(); i++){
 			if (A[i] == '"'){
 				if (change == 0){
@@ -21,9 +19,5 @@ int main(){
 			}
 		}
 		cout << A << endl;
-
-
 	}
-
-
 }

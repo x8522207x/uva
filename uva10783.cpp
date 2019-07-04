2c015d8c72fa62dafbe472b9b562b24b12184d38
x8522207x <x8522207x@gmail.com>
2019-07-04 11:35:54 +0800

    Odd Sum

--- uva10783.cpp
+++ uva10783.cpp
@@ -1,21 +1,17 @@
 #include <iostream>
 using namespace std;
-int main() {
-	int Case;
-	cin >> Case;
-	for(int c=0;c<Case;c++){
-		int a, b;
-		int sum = 0;
-		cin >> a >> b;
-		for (int i = a; i <= b; i = i + 2) {
-			if (i % 2 == 0) {
-				i++;
+int main(){
+	int Case=0;
+	cin>>Case;
+	for(int j=1;j<=Case;j++){
+		int num1=0,num2=0,ans=0;
+		cin>>num1>>num2;
+		for(int i=num1;i<=num2;i+=2){
+			if(i%2!=1){
+				i+=1;
 			}
-			sum = sum + i;
+			ans+=i;
 		}
-		cout << "Case " << c + 1 << ": "<<sum<<endl;
+		cout<<"Case "<<j<<": "<<ans<<endl;
 	}
-
-
-
 }

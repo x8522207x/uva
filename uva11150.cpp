f2dc919f58e7212627b57f1c5be0db17bf7a45ae
x8522207x <x8522207x@gmail.com>
2019-07-04 21:02:11 +0800

    Cola

--- uva11150.cpp
+++ uva11150.cpp
@@ -1,29 +1,9 @@
 #include <iostream>
 using namespace std;
-int main() {
-	int cola = 0;
-
-	while (cin >> cola) {
-		int air = 0;
-		int ans = 0;
-		int borrow = 0;
-		if (cola % 3 == 2) {
-			borrow++;
-			air++;
-		}
-		for(int i=0;;i++){
-			ans += cola;
-			air += cola;
-			cola = 0;
-			if (air < 3) {
-				if (air == 2 && borrow == 0) {
-					ans++;
-				}
-				break;
-			}
-			cola = air / 3;
-			air = air - air / 3 * 3;
-		}
-		cout <<ans << endl;
+int main(){
+	int nums=0;
+	while(cin>>nums){
+		int ans=nums*1.5;
+		cout<<ans<<endl;
 	}
 }

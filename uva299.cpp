b3dd7f901f547cfafdfc69931a76ec021978a2ba
x8522207x <x8522207x@gmail.com>
2019-07-05 22:36:24 +0800

    Train Swapping

---
+++ uva299.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+	int Case=0;
+	cin>>Case;
+	while(Case--){
+		int count=0, swapT=0;
+		cin>>count;
+		vector<int>num;
+		for(int i=0,c=0;i<count;i++){
+			cin>>c;
+			num.push_back(c);
+		}
+		for(int i=0;i<num.size()-1;i++){
+			for(int j=i+1;j<num.size();j++){
+				if(num[i]>num[j]){
+					swap(num[i],num[j]);
+					swapT++;
+				}
+			}
+		}
+		cout<<"Optimal train swapping takes "<<swapT<<" swaps."<<endl;
+	}
+}

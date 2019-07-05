5096ee90c68789d52e2f13bbe967d0cfcaef54d1
x8522207x <x8522207x@gmail.com>
2019-07-05 19:44:25 +0800

    Funny Encryption Method

--- uva10019.cpp
+++ uva10019.cpp
@@ -1,59 +1,40 @@
 #include <iostream>
 #include <vector>
-#include <string>
 #include <cmath>
 using namespace std;
-int main() {
-	int Case;
-	int two = 1;
-	vector<int>twoall;
-	for (int i = 0;; i++) {
-		twoall.push_back(two);
-		two *= 2;
-		if (two > 99999) {
-			break;
-		}
+int main(){
+	int Case=0;
+	cin>>Case;
+	vector<int>bin;
+	for(int i=15;i>=0;i--){
+		bin.push_back(pow(2,i));
 	}
-	cin >> Case;
-	while (Case--) {
-		int b1 = 0;
-		int b2 = 0;
-		vector<int>hex;
-		int number;
-		int numberhex;
-		cin >> number;
-		numberhex = number;
-		/*
-		to_string another method
-		
-		stringstream ss;
-		ss<<numberhex;
-		string s=ss.str();
-		
-		*/
-		for (int i = 0; i < to_string(numberhex).size(); i++) {
-			hex.push_back((to_string(numberhex)[i] - '0'));
-		}
-		numberhex = 0;
-		for (int i = 0, j = hex.size() - 1; i < hex.size(), j >= 0; i++, j--) {
-			numberhex += hex[i] * pow(16, j);
-		}
-
-		for (int i = twoall.size() - 1; i >= 0; i--) {
-			if (twoall[i] <= number) {
-				number -= twoall[i];
-				b1++;
+	while(Case--){
+		int num=0;
+		cin>>num;
+		int binN=1;
+		int hex=num;
+		for(int i=0;i<bin.size();i++){
+			if(num>=bin[i]){
+				binN++;
+				num-=bin[i];
 			}
 		}
-		for (int i = twoall.size() - 1; i >= 0; i--) {
-			if (twoall[i] <= numberhex) {
-				//cout << twoall[i] << " " << numberhex << endl;
-				numberhex -= twoall[i];
-				b2++;
+		cout<<binN-1<<" ";
+		int dec=0;
+		int powN=0;
+		while(hex!=0){
+			dec+=(hex%10)*pow(16,powN);
+			powN++;
+			hex/=10;
+		}
+		binN=1;
+		for(int i=0;i<bin.size();i++){
+			if(dec>=bin[i]){
+				binN++;
+				dec-=bin[i];
 			}
 		}
-		cout << b1 << " " << b2 << endl;
+		cout<<binN-1<<endl;
 	}
-	system("pause");
-	return 0;
 }

4c3f5c05154bfbaa04c2daf54d6e4905f01587ec
x8522207x <x8522207x@gmail.com>
2019-07-05 17:59:27 +0800

    Square Numbers

--- uva11461.cpp
+++ uva11461.cpp
@@ -1,25 +1,16 @@
 #include <iostream>
-
 using namespace std;
-int main() {
-	int a, b;
-	while (cin >> a >> b) {
-		if (a == 0 && b == 0) {
-			break;
-		}
-		int numbers=0;
-		for (int i = 1; ; i++) {
-			if (i*i >= a&&i*i <= b) {
-				numbers++;
-			}
-			else if (i*i > b) {
+int main(){
+	int firstN=0,secondN=0;
+	while(cin>>firstN>>secondN&&firstN!=0&&secondN!=0){
+		int ans=0;
+		for(int i=1;;i++){
+			if(i*i>=firstN&&i*i<=secondN){
+				ans++;
+			}else if(i*i>secondN){
 				break;
 			}
 		}
-		cout << numbers << endl;
-
+		cout<<ans<<endl;
 	}
-
-
-	return 0;
 }

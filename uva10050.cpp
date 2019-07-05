8b275530354d9825b6a47045f683f0ae959f6c05
x8522207x <x8522207x@gmail.com>
2019-07-05 20:57:12 +0800

    Hartals

---
+++ uva10050.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main(){
+	int Case=0;
+	cin>>Case;
+	while(Case--){
+		int days=0;
+		int party=0;
+		int ans=1;
+		vector<int>rest;
+		vector<int>workday;
+		cin>>days>>party;
+		for(int i=0;i<=days/7;i++){
+			rest.push_back(6+7*i);
+			rest.push_back(7+7*i);
+		}
+		for(int i=0,f=0;i<party;i++){
+			cin>>f;
+			for(int c=1;c<=days/f;c++){
+				bool same =false;
+				for(int j=0;j<rest.size();j++){
+					if(rest[j]==c*f){
+						same=true;
+						break;
+					}
+				}
+				if(same==false){
+					workday.push_back(c*f);
+				}
+			}
+		}
+		sort(workday.begin(),workday.end());
+		for(int i=1;i<workday.size();i++){
+			if(workday[i]!=workday[i-1]){
+				ans++;
+			}
+		}
+		cout<<ans<<endl;
+	}
+}

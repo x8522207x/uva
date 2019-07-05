ce270c871d2c13acaba004d4d91ce3027f7c7b74
x8522207x <x8522207x@gmail.com>
2019-07-05 22:27:55 +0800

    Simply Emirp

---
+++ uva10235.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <sstream>
+using namespace std;
+int main(){
+	string s,b;
+	while(cin>>s){
+		string b;
+		int fN=0,sN=0;
+		stringstream ss;
+		bool end=false;
+		for(int i=s.size()-1;i>=0;i--){
+			b+=s[i];
+		}
+		ss<<s;
+		ss>>fN;
+		ss.clear();
+		ss<<b;
+		ss>>sN;
+		ss.clear();
+		for(int i=2;i<=fN/2;i++){
+			if(fN%i==0){
+				cout<<fN<<" is not prime."<<endl;
+				end=true;
+				break;
+			}
+		}
+		if(end==false){
+			if(sN==fN){
+				cout<<fN<<" is prime."<<endl;
+			}else{
+				for(int i=2;i<sN/2;i++){
+					if(sN%i==0){
+						cout<<fN<<" is prime."<<endl;
+						end=true;
+						break;
+					}
+				}
+				if(end==false){
+					cout<<fN<<" is emirp."<<endl;
+				}
+			}
+		}
+	}
+}

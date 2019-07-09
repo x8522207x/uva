2387b3ddd7dbb35aabd6f49778877c30384a35e5
x8522207x <x8522207x@gmail.com>
2019-07-09 16:30:56 +0800

    Jumping Mario

--- uva11764.cpp
+++ uva11764.cpp
@@ -4,18 +4,15 @@ using namespace std;
 int main(){
 	int Case=0;
 	cin>>Case;
-	
 	for(int g=0;g<Case;g++){
-		int times=0;
-		cin>>times;
+		int times=0,high=0,low=0;
 		vector<int>step;
+		cin>>times;
 		for(int i=0;i<times;i++){
 			int walls=0;
 			cin>>walls;
 			step.push_back(walls);
 		}
-		int high=0;
-		int low=0;
 		for(int i=1;i<step.size();i++){
 			if(step[i]-step[i-1]>0){
 				high+=1;
@@ -25,5 +22,4 @@ int main(){
 		}
 		cout<<"Case "<<g+1<<": "<<high<<" "<<low<<endl;
 	}
-	return 0;
 }

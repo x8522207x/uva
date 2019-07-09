9c1c17a70b2852731897da8a8267b14ce14b5f3e
x8522207x <x8522207x@gmail.com>
2019-07-09 15:44:24 +0800

    What is the Probability?

--- uva10056.cpp
+++ uva10056.cpp
@@ -6,9 +6,8 @@ int main(){
 	int Case;
 	cin >> Case;
 	while (Case--){
-		int players;
-		long double probability;
-		int I;
+		int players=0,I=0;
+		long double probability=0.0;
 		cin >> players >> probability >> I;
 		if (probability > 0.0){
 			cout << fixed << setprecision(4) << probability*pow(1 - probability, I - 1) / (1 - pow(1 - probability, players));
@@ -18,5 +17,4 @@ int main(){
 		}
 		cout << endl;
 	}
-	return 0;
 }

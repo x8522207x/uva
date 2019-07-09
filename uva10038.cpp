a88c7e8b72f08523db8f663c65d69221721cc5c8
x8522207x <x8522207x@gmail.com>
2019-07-09 15:40:18 +0800

    Jolly Jumpers

--- uva10038.cpp
+++ uva10038.cpp
@@ -3,15 +3,14 @@
 #include <algorithm>
 using namespace std;
 int main(){
-	int num;
+	int num=0;
 	while(cin>>num){
-		vector<int>numbers;
-		for(int i=0;i<num;i++){
-			int g;
+		int count=0;
+		vector<int>numbers,answer;
+		for(int i=0,g=0;i<num;i++){
 			cin>>g;
 			numbers.push_back(g);
 		}
-		vector<int>answer;
 		for(int i=1;i<numbers.size();i++){
 			if(numbers[i]-numbers[i-1]<0){
 				answer.push_back(-numbers[i]+numbers[i-1]);
@@ -20,7 +19,6 @@ int main(){
 			}
 		}
 		sort(answer.begin(),answer.end());
-		int count=0;
 		for(int i=0;i<answer.size();i++){
 			if(i+1!=answer[i]){
 				cout<<"Not jolly"<<endl;
@@ -31,8 +29,5 @@ int main(){
 		if(count==0){
 			cout<<"Jolly"<<endl;
 		}
-	
 	}
-
-	return 0;
 }

02d2d64612f18b5108f4c1cbf32e0433a68211f6
x8522207x <x8522207x@gmail.com>
2019-07-09 16:09:30 +0800

    You can say 11

--- uva10929.cpp
+++ uva10929.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 #include <string>
-#include<cmath>
+#include <cmath>
 using namespace std;
 int main(){
 	string input;
 	while (cin >> input&&input != "0"){
-		int odd = 0;
-		int even = 0;
+		int odd = 0,even = 0, sum=0;
 		for (int i = 0; i < input.length(); i = i + 2){
 			odd = odd + input[i] - '0';
 		}
 		for (int i = 1; i < input.length(); i = i + 2){
 			even = even + input[i] - '0';
 		}
-		if (abs(odd - even) % 11 == 0){
+		sum = abs(odd - even);
+		if (sum % 11 == 0){
 			cout << input << " is a multiple of 11." << endl;
+		}else{ 
+			cout << input << " is not a multiple of 11." << endl; 
 		}
-		else{ cout << input << " is not a multiple of 11." << endl; }
 	}
 } 

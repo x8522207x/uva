58c594cb5bb87e2828b90d77ac29d17d4c3c6fba
x8522207x <x8522207x@gmail.com>
2019-07-09 15:46:20 +0800

    An Easy Problem!

--- uva10093.cpp
+++ uva10093.cpp
@@ -4,6 +4,7 @@ int main(){
 	string s;
 	while(cin>>s){
 		int ans=0;	
+		bool mod=false;
 		for(int i=0;i<s.length();i++){
 			if(s[i]>47&&s[i]<58){
 				ans+=s[i]-'0';
@@ -13,7 +14,6 @@ int main(){
 				ans+=s[i]-61;
 			}
 		}
-		bool mod=false;
 		if(ans>=10){
 			for(int i=61;i>=2;i--){
 				if(ans%i==0){

e2c78ae9f225083364ddbbb165eccdf15e0de909
x8522207x <x8522207x@gmail.com>
2019-07-09 16:06:46 +0800

    Beat the Spread!

--- uva10812.cpp
+++ uva10812.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int Case =0;
+	int Case =0,a=0, b=0;
 	cin >> Case;
 	while (Case--) {
-		int a, b;
 		cin >> a >> b;
 		if ((a - b) / 2 >= 0&&((a + b) / 2+(a - b) / 2)==a) {
 			cout << (a + b) / 2 << " " << (a - b) / 2 << endl;
@@ -13,5 +12,4 @@ int main() {
 			cout << "impossible" << endl;
 		}
 	}
-	return 0;
 }

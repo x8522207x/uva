1f0b0320814949a28a5a9024c1f23e48b4abca76
x8522207x <x8522207x@gmail.com>
2019-07-09 17:49:13 +0800

    WERTYU

---
+++ uva10082.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+	string before="][POIUYTREW';LKJHGFDS/.,MNBVCX=-0987654321 ", after="[POIUYTREWQ;LKJHGFDSA.,MNBVCXZ-0987654321` ", s;
+	while(getline(cin,s)){
+		for(int i=0;i<s.length();i++){
+			for(int j=0;j<before.length();j++){
+				if(s[i]==before[j]){
+					s[i]=after[j];
+					break;
+				}
+			}
+		}
+		cout<<s<<endl;
+	}
+}

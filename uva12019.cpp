038eaf741e37d719749e73af20fb4d6fb7f397fa
x8522207x <x8522207x@gmail.com>
2019-07-09 16:34:45 +0800

    Doom's Day Algorithm

--- uva12019.cpp
+++ uva12019.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 using namespace std;
 int main(){
-	int days[12]={31,28,31,30,31,30,31,31,30,31,30,31};
-	int Case=0;
+	int days[12]={31,28,31,30,31,30,31,31,30,31,30,31},Case=0;
 	string week[7]={"Saturday","Sunday","Monday","Tuesday","Wednesday","Thursday","Friday"};
 	cin>>Case;
 	while(Case--){//   1/1 星期六
-		int month=0;
-		int day=0;
+		int month=0,day=0;
 		cin>>month>>day;
 		for(int i=0;i<month-1;i++){
 			day+=days[i];

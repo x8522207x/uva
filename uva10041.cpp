301a2e374c6841fd33bf22e0dd05a88ea99a4a7e
x8522207x <x8522207x@gmail.com>
2019-07-09 15:41:38 +0800

    Vito'sfamily

--- uva10041.cpp
+++ uva10041.cpp
@@ -6,7 +6,7 @@ int main(){
 	int cases=0;
 	cin>>cases;
 	for(int g=0;g<cases;g++){
-		int numbers=0;
+		int numbers=0,sum=0,middle=0;
 		vector<int>mid;
 		cin>>numbers;
 		for(int i=0;i<numbers;i++){
@@ -15,14 +15,11 @@ int main(){
 			mid.push_back(number);
 		}
 		sort(mid.begin(),mid.end());
-		int middle=0;
 		if(numbers%2==0){
 			middle=(mid.at(numbers/2-1)+mid.at(numbers/2))/2;
 		}else{
 			middle=mid.at(numbers/2);
 		}
-
-		int sum=0;
 		for(int i=0;i<mid.size();i++){
 			if(middle-mid.at(i)<0){
 				sum+=mid.at(i)-middle;
@@ -32,6 +29,4 @@ int main(){
 		}
 		cout<<sum<<endl;
 	}
-	return 0;
-
 }

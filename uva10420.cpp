d17e218b93c6530ead7c25a768db51c930c67024
x8522207x <x8522207x@gmail.com>
2019-07-09 16:04:06 +0800

    List of Conquests

--- uva10420.cpp
+++ uva10420.cpp
@@ -3,22 +3,20 @@
 #include <algorithm>
 using namespace std;
 int main(){
-	int Case=0;
+	int Case=0,ans=0;
+	string s2;
+	vector<string>sv;
 	cin>>Case;
 	cin.ignore();
-	vector<string>sv;
 	while(Case--){
-		string s;
+		string s,g;
 		getline(cin,s);
-		string g;
 		for(int i=0;i<s.find_first_of(" ");i++){
 			g+=s[i];
 		}
 		sv.push_back(g);
 	}
 	sort(sv.begin(),sv.end());
-	int ans=0;
-	string s2;
 	s2=sv[0];
 	for(int i=1;i<sv.size();i++){
 		if(s2==sv[i]){

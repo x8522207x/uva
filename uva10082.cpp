b7c88c393b356a1a0c5b1af87f8fc91d8863ace7
x8522207x <x8522207x@gmail.com>
2019-07-11 14:49:58 +0800

    WERTYU

--- uva10082.cpp
+++ uva10082.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 int main(){
-	string before="][POIUYTREW';LKJHGFDS/.,MNBVCX=-0987654321 ", after="[POIUYTREWQ;LKJHGFDSA.,MNBVCXZ-0987654321` ", s;
+	string before="\\][POIUYTREW';LKJHGFDS/.,MNBVCX=-0987654321 ", after="][POIUYTREWQ;LKJHGFDSA.,MNBVCXZ-0987654321` ", s;
 	while(getline(cin,s)){
 		for(int i=0;i<s.length();i++){
 			for(int j=0;j<before.length();j++){

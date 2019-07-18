6028a3fcf55b041fe9a58840b263209d2f0a5b2c
x8522207x <x8522207x@gmail.com>
2019-07-18 21:12:14 +0800

    Satellites

--- uva10221.cpp
+++ uva10221.cpp
@@ -10,21 +10,14 @@ int main(){
 		s+=6440;
 		if(g =="min"){
 			a/=60;
-			if(a>180){
-				int num=a/180;
-				a-=180*num;
-			}
-			double arc = 2*s*pi*a/360;
-			double chord = 2*s*sin(a/2/180*pi);
-			cout<<fixed<<setprecision(6)<<arc<<" "<<chord<<endl;
-		}else {
-			if(a>180){
-				int num=a/180;
-				a-=180*num;
-			}
-			double arc = 2*s*pi*a/360;
-			double chord = 2*s*sin(a/2/180*pi);
-			cout<<fixed<<setprecision(6)<<arc<<" "<<chord<<endl;
+			
 		}
+		if(a>180){
+			int num=a/180;
+			a-=180*num;
+		}
+		double arc = 2*s*pi*a/360;
+		double chord = 2*s*sin(a/2/180*pi);
+		cout<<fixed<<setprecision(6)<<arc<<" "<<chord<<endl;
 	}
 }

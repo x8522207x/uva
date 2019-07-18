6060a67041fce744dc833803fb8d5f8b0c126d08
x8522207x <x8522207x@gmail.com>
2019-07-18 21:10:04 +0800

    Satellites

---
+++ uva10221.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+#define pi 2*acos(0)
+int main(){
+	double s=0.0, a=0.0;
+	string g;
+	while(cin>>s>>a>>g){
+		s+=6440;
+		if(g =="min"){
+			a/=60;
+			if(a>180){
+				int num=a/180;
+				a-=180*num;
+			}
+			double arc = 2*s*pi*a/360;
+			double chord = 2*s*sin(a/2/180*pi);
+			cout<<fixed<<setprecision(6)<<arc<<" "<<chord<<endl;
+		}else {
+			if(a>180){
+				int num=a/180;
+				a-=180*num;
+			}
+			double arc = 2*s*pi*a/360;
+			double chord = 2*s*sin(a/2/180*pi);
+			cout<<fixed<<setprecision(6)<<arc<<" "<<chord<<endl;
+		}
+	}
+}
